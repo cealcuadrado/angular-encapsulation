4a7eedef2b7264f4fd46dbd02c3942b56ae43493
CeAlCuadrado <cealcuadrado@outlook.com>
2021-07-05 15:23:06 -0400

    Creado layout de encapsulación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { EmulatedEncapsulationComponent } from './shared/emulated-encapsulation/emulated-encapsulation.component';
+import { NoEncapsulationComponent } from './shared/no-encapsulation/no-encapsulation.component';
+import { ShadowdomEncapsulationComponent } from './shared/shadowdom-encapsulation/shadowdom-encapsulation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    EmulatedEncapsulationComponent,
+    NoEncapsulationComponent,
+    ShadowdomEncapsulationComponent
   ],
   imports: [
     BrowserModule
